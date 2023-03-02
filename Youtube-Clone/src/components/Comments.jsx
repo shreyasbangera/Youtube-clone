c0a51571cb854969447fb79013b31a488876f4a4
shreyasbangera <shreyasybangera1@gmail.com>
2023-03-02 15:55:03 +0530

    updated comment section

--- Youtube-Clone/src/components/Comments.jsx
+++ Youtube-Clone/src/components/Comments.jsx
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { COMMENTS_API } from '../utils/constants'
+import { BiLike, BiDislike } from "react-icons/bi";
+import ReactHtmlParser from 'react-html-parser'
+import { FaRegUserCircle } from 'react-icons/fa';
 
 const Comments = ({id, commentCount}) => {
 
@@ -13,13 +16,18 @@ const Comments = ({id, commentCount}) => {
   const fetchComments = async() => {
     const formattedUrl = COMMENTS_API.replace("VIDEO_ID", id)
     const res = await axios.get(formattedUrl)
-    setComments(res.data.items)
-    
+    setComments(res.data.items)   
   }
+
+  console.log(comments)
+
   return (
     <div className='flex flex-col gap-2'>
     <h2 className='font-normal'>{commentCount} Comments</h2>
-    <input type="text" placeholder='Add a comment...' className='border-b-2 outline-none' />
+    <div className='flex gap-3 items-center'>
+    <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" className='w-10'  />
+    <input type="text" placeholder='Add a comment...' className='border-b-2 outline-none w-full' />
+    </div>
     <div className='flex flex-col gap-5 mt-5'>
     {comments?.map((comment)=> {
       const {snippet} = comment
@@ -27,11 +35,16 @@ const Comments = ({id, commentCount}) => {
       const {id} = topLevelComment
       return(
     <div className='text-sm flex flex-col' key={id}>
-    <div className='font-semibold flex items-start p-2'>
-    <img src ={topLevelComment.snippet.authorProfileImageUrl} alt="Profile image" className='rounded-full w-[40px] h-[40px]' />
+    <div className='font-semibold flex items-start pt-2'>
+    <img src ={topLevelComment.snippet.authorProfileImageUrl} alt="Profile" className='rounded-full w-[40px] h-[40px]' onError={(e)=> e.target.src="https://cdn-icons-png.flaticon.com/512/149/149071.png"}/>
     <div className='flex flex-col ml-3 gap-1'>
     <p>{topLevelComment.snippet.authorDisplayName}</p>
-    <p className='font-normal'>{topLevelComment.snippet.textDisplay}</p>
+    <p className='font-normal'>{ReactHtmlParser(topLevelComment.snippet.textDisplay)}</p>
+    <div className='flex gap-4 mt-2 items-center'>
+      <button className='flex font-normal text-sm items-center gap-2'><BiLike size={20}/>{topLevelComment.snippet.likeCount}</button>
+      <button><BiDislike size={20}/></button>
+      <button>Reply</button>
+    </div>
     </div>
     </div>
     </div>
